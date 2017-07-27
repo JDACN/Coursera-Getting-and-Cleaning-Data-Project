3eb2f5682163450117c57d4572eb6134226c3be2
Jayant Dhawale <jayant.dhawale@accenture.com>
2017-07-27 15:55:12 +0530

    Final upload  after bug fix.

--- run_analysis.R
+++ run_analysis.R
@@ -35,25 +35,21 @@ y_data <- rbind(y_train, y_test)
 # create 'subject' data set
 subject_data <- rbind(subject_train, subject_test)
 
-# Step 2
-# Extract only the measurements on the mean and standard deviation for each measurement
-###############################################################################
 
+# Extract only the measurements on the mean and standard deviation for each measurement
 features <- read.table("UCI HAR Dataset/features.txt")
 
 # get only columns with mean() or std() in their names
-mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2]) #features.txt we need 2nd col only , filter data
 
 # subset the desired columns
 x_data <- x_data[, mean_and_std_features]
 
-# correct the column names
+# Assign the column names from features.txt
 names(x_data) <- features[mean_and_std_features, 2]
 
-# Step 3
-# Use descriptive activity names to name the activities in the data set
-###############################################################################
 
+# Use descriptive activity names to name the activities in the data set
 activities <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 # update values with correct activity names
@@ -62,24 +58,22 @@ y_data[, 1] <- activities[y_data[, 1], 2]
 # correct column name
 names(y_data) <- "activity"
 
-# Step 4
+
 # Appropriately label the data set with descriptive variable names
-###############################################################################
 
 # correct column name
 names(subject_data) <- "subject"
 
-# bind all the data in a single data set
+# bind all the data in a single data table
 all_data <- cbind(x_data, y_data, subject_data)
 
-# Step 5
-# Create a second, independent tidy data set with the average of each variable
+
+
 # for each activity and each subject
-###############################################################################
 
 # 66 <- 68 columns but last two (activity & subject)
 averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
 
-write.table(averages_data, "averages_data.txt", row.name=FALSE)
+write.table(averages_data, "output.txt", row.name=FALSE)
 
 
